7d34cf10d88b6dfff8d3e2327c46e5602385114e
jungeun826 <jungeun826@nate.com>
2014-01-06 15:12:11 +0900

    실습 150페이지를 완료했다.
    label을 1개 더 추가하여 각각의 component에 따라 어떤 row 와 항목을 가지는지 출력하는 것을 추가해 보고싶다.

--- PickerVIew/ViewController.m
+++ PickerVIew/ViewController.m
@@ -8,16 +8,39 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIPickerViewDelegate, UIPickerViewDataSource>{
+    NSArray *data;
+}
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
 @implementation ViewController
+-(NSInteger )numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 2;
+}
+-(NSInteger )pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    if(component == 0)
+        return 10;
+    else
+        return [data count];
+}
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    if(component == 0)
+        return [NSString stringWithFormat:@"Row : %d", (int)row];
+    else
+        return [data objectAtIndex:row];
 
+}
+-(void) pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+    NSString *item = [self pickerView:pickerView titleForRow:row forComponent:component];
+    self.label.text = [NSString stringWithFormat:@"컴포넌트:%d, 로우:%d, 항목:%@",(int)component,(int)row,item];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    data = [[NSArray alloc]initWithObjects:@"Item1",@"Item2",@"Item3",@"Item4",@"Item5", nil];
 }
 
 - (void)didReceiveMemoryWarning
